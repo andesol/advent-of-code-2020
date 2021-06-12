b61ca8fe402c70e50bc64ac5e6ba827b1dfbd365
Andreu <anddessol@gmail.com>
2021-06-12 23:25:14 +0200

    update test

--- day-2/2.test.js
+++ day-2/2.test.js
@@ -1,25 +1,30 @@
 const isRighPosition = require('./2.js');
 
-test('check if the position is as expected', () => {
-    let lower = 1;
-    let upper = 3;
-    let letter = 'a';
-    let password = 'abcde'
+describe('check if the position is as expected', () => {
 
-    expect(isRighPosition({lower, upper, letter, password})).toBe(true);
+    test('case 1', () => {
+        const lower = 1;
+        const upper = 3;
+        const letter = 'a';
+        const password = 'abcde'
 
+        expect(isRighPosition({lower, upper, letter, password})).toBe(true);
+    })
 
-    lower = 1;
-    upper = 3;
-    letter = 'b';
-    password = 'cdefg'
+    test('case 2', () => {
+        const lower = 1;
+        const upper = 3;
+        const letter = 'b';
+        const password = 'cdefg'
 
-    expect(isRighPosition({lower, upper, letter, password})).toBe(false);
+        expect(isRighPosition({lower, upper, letter, password})).toBe(false);
+    })
 
-    lower = 2;
-    upper = 9;
-    letter = 'c';
-    password = 'ccccccccc'
-
-    expect(isRighPosition({lower, upper, letter, password})).toBe(false);
+    test('case 3', () => {
+        const lower = 2;
+        const upper = 9;
+        const letter = 'c';
+        const password = 'ccccccccc'
+        expect(isRighPosition({lower, upper, letter, password})).toBe(false);
+    })
 })
